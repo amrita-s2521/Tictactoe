5a706635ad259b15bde54f9f42eb2ec2d90c6553
amrita-s2521 <as2521@srmist.edu.in>
2026-04-20 09:21:57 +0530

    Add UC1

---
+++ TicTacToe.java
@@ -0,0 +1,30 @@
+package com.srm.src;
+public class TicTacToe {
+    // Corrected array declaration
+    static char[][] board = new char[3][3];
+
+    public static void main(String[] args) {
+        initializeBoard();
+        printBoard();
+    }
+
+    static void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' '; // Fill with empty spaces
+            }
+        }
+    }
+
+    static void printBoard() {
+        System.out.println("-------------");
+        for (int row = 0; row < 3; row++) {
+            System.out.print("| ");
+            for (int col = 0; col < 3; col++) {
+                System.out.print(board[row][col] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+}
