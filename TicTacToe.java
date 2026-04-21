ee273391650817533f8434d517802ca5f86fe918
amrita-s2521 <as2521@srmist.edu.in>
2026-04-21 11:05:40 +0530

    UC4

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,28 +1,17 @@
 package com.srm.src;
-import java.util.Scanner;
 public class TicTacToe {
+
     public static void main(String[] args) {
-        int slot = getUserSlot();
-        System.out.println("Slot entered: " + slot);
+        int slot = 7;
+        System.out.println("Row: " + getRowFromSlot(slot));
+        System.out.println("Column: " + getColFromSlot(slot));
     }
-    static int getUserSlot() {
-        Scanner scan = new Scanner(System.in);
-        int slot;
-        while (true) {
-            System.out.print("Enter a slot number (1-9): ");
-            if (scan.hasNextInt()) {
-                slot = scan.nextInt();
-                if (slot >= 1 && slot <= 9) {
-                    break; 
-                } else {
-                    System.out.println("Invalid input. Please enter a number between 1 and 9.");
-                }
-            } else {
-                System.out.println("Invalid input. Please enter a number between 1 and 9.");
-                scan.next(); 
-            }
-        }
-        scan.close();
-        return slot;
+
+    static int getRowFromSlot(int slot) {
+        return (slot - 1) / 3;
+    }
+
+    static int getColFromSlot(int slot) {
+        return (slot - 1) % 3;
     }
 }
