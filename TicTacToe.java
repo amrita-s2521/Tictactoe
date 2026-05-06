d8a6d7ddad64c33c427cfa7440b0fabcf60e245e
amrita-s2521 <as2521@srmist.edu.in>
2026-05-06 08:38:08 +0530

    Add TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,17 +1,13 @@
 package com.srm.src;
-
 import java.util.Scanner;
-
 public class TicTacToe {
     static char[][] board = {
         {'-', '-', '-'},
         {'-', '-', '-'},
         {'-', '-', '-'}
     };
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         // UC5 Flow: Receive Row and Column
         System.out.println("Enter move (row and column: 0-2): ");
         int row = scanner.nextInt();
@@ -27,7 +23,6 @@ public class TicTacToe {
             System.out.println("Invalid move! Cell is out of bounds or already occupied.");
         }
     }
-
     public static boolean isValidMove(int row, int col) {
         // Boundary check
         if (row < 0 || row >= 3 || col < 0 || col >= 3) {
